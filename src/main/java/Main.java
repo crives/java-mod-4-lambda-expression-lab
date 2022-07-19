998d254ffc14611dfd42024c6864bedc9929dd8d
Caroline Rives <car.rives@gmail.com>
2022-07-19 14:25:37 -0400

    Lambda expression lab complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,18 @@
 import java.util.function.IntUnaryOperator;
 
 public class Main {
+    /*
+    Write a lambda expression that takes an integer and returns the next odd number.
+    The expression should be public and static.
+     */
 	// write the nextOddNum here
-    
+    @FunctionalInterface
+    interface NextOddNum {
+        int applyAsInt(int n);
+    }
+
+    public static NextOddNum nextOddNum = (int n) -> (n % 2 == 0) ? n + 1 : n + 2;
+
     public static void main(String[] args) {
         int num = 5;
         int num2 = 10;
